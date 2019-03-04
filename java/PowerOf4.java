4ae8a4eb1f84145c45e9617b4493cef880b58fa3
Namita <aroranamita33@gmail.com>
2019-03-04 14:22:04 +0530

    corrected

--- java/PowerOf4.java
+++ java/PowerOf4.java
@@ -3,7 +3,7 @@ package com.stackroute;
 import java.util.Scanner;
 
 public class PowerOf4 {
-    public int checkPowerOf4(int num)
+    public boolean checkPowerOf4(int num)
     {
         
         int resultNumInt = (int)Math.log(num)/(int)Math.log(4);
@@ -11,11 +11,13 @@ public class PowerOf4 {
         if(resultNumInt == resultNumDouble)
         {
             System.out.println("Number is power of 4");
+            return true;
         }
         else
         {
             System.out.println("Number is not a power of 4");
+            return false;
         }
-        return resultNumInt;
+       
     }
 }
